a0126ac28b0672303d2f655bdc4ae7127f20dd39
AudreyJones <audrey.eliz.jones@gmail.com>
2019-07-15 15:13:06 -0400

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -13,6 +13,10 @@ class CreateTodo extends Component {
   handleSubmit = event => {
     event.preventDefault();
     this.props.addTodo(this.state)
+    // Resetting state each time component is created, to clear input.
+    this.setState({
+      text: '',
+    })
   }
 
   handleChange(event) {
